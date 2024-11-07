02383ae385004874c527f1d463629045827317eb
Daniel <dolaes@ucsc.edu>
2024-11-06 19:00:13 -0800

    Component testing done

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -36,7 +36,7 @@ describe('<Quiz />', () => {
         });
     });
 
-    it('should complete the quiz with all the correct answer ', () => {
+    it('should complete the quiz with all the correct answer', () => {
         cy.get('button').contains('Start Quiz').click();
 
         cy.fixture('../fixtures/question.json').then((questions) => {
@@ -46,6 +46,35 @@ describe('<Quiz />', () => {
             });
     
             cy.get('h2').contains('Quiz Completed').should('be.visible');
+            cy.get('div').contains('Your score: 10/10').should('be.visible');
+        });
+    });
+
+    it('should complete the quiz with all the incorrect answer', () => {
+        cy.get('button').contains('Start Quiz').click();
+
+        cy.fixture('../fixtures/question.json').then((questions) => {
+            questions.forEach((question : Question) => {
+                const incorrectAnswerIndex = (question.answers.findIndex((answer: Answer) => !answer.isCorrect) + 1).toString();
+                cy.get('button').contains(incorrectAnswerIndex).click();
+            });
+    
+            cy.get('h2').contains('Quiz Completed').should('be.visible');
+            cy.get('div').contains('Your score: 0/10').should('be.visible');
+        });
+    });
+
+    it('should restart the quiz in the user wants clicks the reset button', () => {
+        cy.get('button').contains('Start Quiz').click();
+
+        cy.fixture('../fixtures/question.json').then((questions) => {
+            questions.forEach((question : Question) => {
+                const correctAnswerIndex = (question.answers.findIndex((answer: Answer) => answer.isCorrect) + 1).toString();
+                cy.get('button').contains(correctAnswerIndex).click();
+            });
+    
+            cy.get('button').contains('Take New Quiz').click();
+            cy.get('h2').contains(questions[0].question).should('be.visible');
         });
     });
 });
